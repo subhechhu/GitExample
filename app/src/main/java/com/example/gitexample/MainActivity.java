88c756e75b0e3f104593a9ad5c8d7f64fa8322f7
subhechhu <subhechhu.khanal@gmail.com>
2021-08-05 07:12:29 +0545

    issue check

--- app/src/main/java/com/example/gitexample/MainActivity.java
+++ app/src/main/java/com/example/gitexample/MainActivity.java
@@ -13,5 +13,6 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         Log.e("TAGGED", "test for new checkin");
+        Log.e("TAGGED", "test for issue");
     }
 }
