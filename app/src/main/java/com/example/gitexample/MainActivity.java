3243e0e6e38db96efce1e6fe7854772c0377cd4b
subhechhu <subhechhu.khanal@gmail.com>
2021-08-05 07:05:10 +0545

    bug fixes

--- app/src/main/java/com/example/gitexample/MainActivity.java
+++ app/src/main/java/com/example/gitexample/MainActivity.java
@@ -1,8 +1,9 @@
 package com.example.gitexample;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import android.util.Log;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +11,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Log.e("TAGGED", "test for new checkin");
     }
 }
